51faf68d9eb1579edd00714c3ec9e4a056785b46
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-01 21:00:09 +0530

    feat: char added

--- C/DataTypes/Int/Short/main.c
+++ C/DataTypes/Int/Short/main.c
@@ -5,7 +5,7 @@ int main()
 {
   short x = 32769;
   unsigned short y = -4;
-  printf("%d", x);//-32767
-  // printf("%u", y);//35532
+  printf("%d\n", x);//-32767
+  printf("%u\n", y);//35532
   return 0;
 }
