9dc80445c37d13bac23834bc82d87372f1c2ef1b
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-03 23:49:44 +0530

    feat: array assignment added

--- C/Arrays/program3.c
+++ C/Arrays/program3.c
@@ -3,9 +3,16 @@
 int main()
 {
   //declaration
-  int arr[5];
+  int arr1[5];
+  //base address
   for(int i = 0; i < 5; i++)
   {
-    printf("%d\n", &arr[i]);
+    printf("%d\n", &arr1[i]);
+  }
+  //array values
+  int arr2[5] = {10, 20, 30, 40, 50};
+  for(int j = 0; j < 5; j++)
+  {
+    printf("%d\n", arr2[j]);
   }
 }
