0bde56d4bea377c3913ffbced269ab658500d6bf
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-04 00:05:04 +0530

    feat: array operations added

--- C/Arrays/program3.c
+++ C/Arrays/program3.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main()
 {
   //declaration
   int arr1[5];
-  //base address
+  
+  //print base address
   for(int i = 0; i < 5; i++)
   {
     printf("%d\n", &arr1[i]);
   }
-  //array values
+
+  //print array values
   int arr2[5] = {10, 20, 30, 40, 50};
   for(int j = 0; j < 5; j++)
   {
     printf("%d\n", arr2[j]);
+  }
+
+  //put values inside array
+  int arr3[5];
+  for(int k = 0; k < 5; k++)
+  {
+    printf("enter a value: ");
+    scanf("%d", &arr3[k]);
+  }
+  for(int l = 0; l < 5; l++)
+  {
+    printf("%d\n", arr3[l]);
   }
 }
