7f8c6651eeae430aeff12b8872894f0afdbdae7f
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-05 02:14:10 +0530

    feat: array insertion added

---
+++ C/Arrays/program5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int arr[50], n, i, j, key, loc;
+  printf("Enter size of array: ");
+  scanf("%d", &n);
+  for(i = 0; i < n; i++)
+  {
+    printf("Enter %d elements: ", n);
+    scanf("%d", &arr[i]);
+  }
+  printf("enter element to insert: ");
+  scanf("%d", &key);
+  printf("enter desired location: ");
+  scanf("%d", &loc);
+  for(i = n - 1; i >= loc; i--)
+  {
+    arr[i + 1] = arr[i];
+  }
+  arr[loc] = key;
+  for(j = 0; j < n; j++)
+  {
+    printf("elements are: %d \n", arr[j]);
+  }
+  return 0;
+}
