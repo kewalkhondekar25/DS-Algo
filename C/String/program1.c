aa7c88647426f032531bf942511fa941e620f5c9
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-06 00:57:27 +0530

    feat: char input added

--- C/String/program1.c
+++ C/String/program1.c
@@ -4,8 +4,14 @@ int main()
 {
   //string 
   char name1[5] = "john";
-  char name2[5] = {'d', 'o', 'e'};
   printf("%s \n", name1);
+  char name2[5] = {'d', 'o', 'e'};
   printf("%s \n", name2);
+
+  //print input name
+  char cityName[5];
+  printf("enter city name: ");
+  scanf("%s", cityName);
+  printf("%s", cityName);
   return 0;
 }
