0ebf652a84d8e8afa051ac007f222ec2b47c7df1
keal khondekar <kewalkhondekar25@gmail.com>
2024-06-07 23:48:51 +0530

    feat: strlen string added

--- C/String/program2.c
+++ C/String/program2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 void main()
 {
   char name[20];
   printf("enter your name: ");
   gets(name);
-  printf("hello %s", name);
+  printf("hello %s \n", name);
+
+  char userName[20];
+  unsigned int len;
+  printf("enter user name: ");
+  gets(userName);
+  len = strlen(userName);
+  printf("length of %s is %d", userName, len);
 }
